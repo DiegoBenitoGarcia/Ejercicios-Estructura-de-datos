39149d1bfa6e29690da2af37651ffc51680db2c5
DiegoBenitoGarcia <diegobenitogarcia26@gmail.com>
2025-10-13 20:22:05 -0500

    Add files via upload

---
+++ Elemento.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasePila
+{
+    internal class Elemento<Tipo>
+    {
+        private Tipo _Value;
+        private Elemento<Tipo> _Next;
+
+        public Elemento(Tipo Value) 
+        {
+            _Value = Value;
+            _Next = null;
+        }
+
+        
+        public Tipo Value
+        {
+            get { return _Value; }
+            set { _Value = value; }
+        }
+        public Elemento<Tipo> Next
+        {
+            get { return _Next; }
+            set { _Next = value; }
+        }
+
+    }
+}
